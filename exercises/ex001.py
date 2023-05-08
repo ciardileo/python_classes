a1e4ff596118aaeaa6464d9ececd2407f891195b
ciardileo <leolopes.ciardi04@gmail.com>
2023-05-08 19:00:01 -0300

    reorganizei a pasta e fiz exercícios de list comprehension

---
+++ exercises/ex001.py
@@ -0,0 +1,25 @@
+"""
+Exercícios de list comprehension do site
+https://bbookman.github.io/Python-list-comprehension1/
+"""
+
+# ex1: find all of the numbers from 1-1000 that are divisible by 7
+lista = [n for n in range(1, 1001) if n % 7 == 0]
+print('=' * 25, '\n', lista)
+
+# ex2: find all of the numbers from 1-1000 that have a 3 in them
+lista = [n for n in range(1, 1001) if "3" in str(n)]
+print('=' * 25, '\n', lista)
+
+
+# ex3: create a list of all the consonantes in the string "Yellow Yaks like yelling and yawning and yesturday they yodled while eating yuky yams"
+
+def is_consonant(word):
+	if "a" in word or "e" in word or "i" in word or "o" in word or "u" in word:
+		return False
+	return True
+
+
+string = "yellow Yaks like yelling and yawning and yesturday they yodled while eating yuky yams"
+lista = [letra for letra in string if is_consonant(letra) if letra.strip() != '']
+print('=' * 25, '\n', lista)
