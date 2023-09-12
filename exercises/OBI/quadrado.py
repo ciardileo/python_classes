bed5ffabef246db6165aa87639c81c9fddf2f165
Léo Ciardi <leolopes.ciardi04@gmail.com>
2023-09-12 15:40:19 -0300

    terminei pqpte do exercício de quadrado mágico

--- exercises/OBI/quadrado.py
+++ exercises/OBI/quadrado.py
@@ -5,6 +5,7 @@ um quadrado mágico é uma matriz n x n inteiros, em que todas as linhas, coluna
 
 def verifica_linhas_colunas():
 	soma_atual = 0
+	soma = 0
 	for linha in quadrado:
 		# soma da linha
 		soma = sum(linha)
@@ -18,11 +19,20 @@ def verifica_linhas_colunas():
 			else:
 				return -1
 
-		return soma
+	return soma
 
 
 def verifica_diagonais():
-	...
+	soma1 = 0
+	soma2 = 0
+	for linha in quadrado:
+		soma1 += linha[quadrado.index(linha)]
+		soma2 += linha[-1 - quadrado.index(linha)]
+
+	if soma1 == soma2:
+		return soma1
+
+	return -1
 
 
 n = int(input())
@@ -31,4 +41,10 @@ quadrado = []
 for i in range(n):
 	quadrado.append([int(x) for x in input().split()])
 
-print(verifica_linhas_colunas())
+soma_diagonais = verifica_diagonais()
+soma_linhas_colunas = verifica_linhas_colunas()
+
+if soma_diagonais == soma_linhas_colunas and soma_diagonais != -1:
+	print(soma_diagonais)
+else:
+	print("-1")
