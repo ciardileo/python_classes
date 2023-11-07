8282f113a3a76b9cad13d7c24f3bee0b87fe6764
ciardileo <leolopes.ciardi04@gmail.com>
2023-11-07 16:29:09 -0300

    django: aprendi as estruturas de repetição e decisão; aprendi a utilizar urls dinâmicas

--- courses/curso_udemy/classes/django/projeto1/blog/views.py
+++ courses/curso_udemy/classes/django/projeto1/blog/views.py
@@ -2,14 +2,40 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# arquivo com json dos blogs
+from blog.data import posts
+
+
 # Create your views here.
 
-context = {
-	'title_blog': "Este é o título do blog"
-}
 
 def blog(request):
-	print("home")
+	print("blog")
+ 
+ 
+	context = {
+		'title_blog': "Este é o título do blog",
+		'posts': posts,
+		'tipo': 68
+	}
+ 
+ 
 	return render(  # renderiza um arquivo html para a view
 		request, "blog.html", context
+	)
+ 
+ 
+def post(request, id):
+	print(f"post {id}")
+ 
+	for post in posts:
+		if post['id'] == id:
+			context = {
+				'title': post['title'],
+				'body': post['body']
+			}
+ 
+ 
+	return render(  # renderiza um arquivo html para a view
+		request, "post.html", context
 	)
--- courses/curso_udemy/classes/django/projeto1/project/urls.py
+++ courses/curso_udemy/classes/django/projeto1/project/urls.py
@@ -25,5 +25,6 @@ from blog import views as blog_views
 urlpatterns = [
 	path('admin/', admin.site.urls),  # linka uma view para a página home/root
 	path('', home_views.home, name="home"),  # cria uma subpágina home e blog e linka o acesso deles com a função da view
-	path('blog/', blog_views.blog, name="blog")
+	path('blog/', blog_views.blog, name="blog"),
+    path('blog/<int:id>', blog_views.post, name='post')
 ]
