896e07f97e62070c4c79288c5f71b77025af78b1
ciardileo <leolopes.ciardi04@gmail.com>
2024-04-10 17:01:35 -0300

    terminei a seção de sql e comecei o projeto2

--- courses/DSA/cap12/aula3.py
+++ courses/DSA/cap12/aula3.py
@@ -13,13 +13,19 @@ cursor = con.cursor()
 cursor.execute("SELECT * FROM tb_vendas_dsa")
 dados = cursor.fetchall()
 df = pd.DataFrame(dados, columns=[description[0] for description in cursor.description])
-print(df.sample(5))
+# print(df.sample(5))
 
 # média
-print(df['Unidades_Vendidas'].mean())
+# print(df['Unidades_Vendidas'].mean())
+
+# # groupby e sorting
+# print(df.groupby("Nome_Produto", sort=True)['Unidades_Vendidas'].mean())
+
+# retorna a média de vendas dos produtos acima de 199 e que tem média de unidades acima de 10
+print(df[df['Valor_Unitario'] > 199].groupby("Nome_Produto").filter(lambda x: x['Unidades_Vendidas'].mean() > 10).groupby("Nome_Produto")['Unidades_Vendidas'].mean())
 
 # contagem de valores
-print(df.value_counts())
+# print(df.value_counts())
 
 # fechamento
 cursor.close()
