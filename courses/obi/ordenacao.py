8b2759d3f3c99333229ede7ae669092c40b210c2
ciardileo <leolopes.ciardi04@gmail.com>
2024-04-18 23:56:29 -0300

    reorganized my studies and studied sorting

---
+++ courses/obi/ordenacao.py
@@ -0,0 +1,36 @@
+"""
+aulão sobre ordenações
+"""
+
+# imports 
+
+import random
+
+# selection sort
+"""
+funciona com uma lógica de ponteiro:
+pegue o primeiro número da lista e comece a comparar com todo o resto, ao final da iteração, caso tenha achado um número menor que o primeiro, troque o primeiro de posicão com esse menor número
+
+"""
+
+# valores
+inteiros_unicos = random.sample(range(1, 101), 25)
+
+print(inteiros_unicos)
+
+def selection_sort(lista):
+    for item_atual in range(len(lista)):
+        menor_indice = item_atual
+        
+        for numero in range(item_atual, len(lista)):
+            if lista[numero] < lista[menor_indice]:
+                menor_indice = numero
+        
+        if menor_indice != item_atual:
+            lista[item_atual], lista[menor_indice] = lista[menor_indice], lista[item_atual]
+            
+    return lista
+        
+print(selection_sort(inteiros_unicos))
+            
+        
